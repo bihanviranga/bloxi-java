d5ea353ebdfe7f9eb77cd46b5a4e38cc87446838
Bihan Viranga <bihanviranga@gmail.com>
2021-11-10 23:57:07 +0530

    Implemented environment nesting for scoped variables

--- src/com/bloxi/lox/Environment.java
+++ src/com/bloxi/lox/Environment.java
@@ -4,7 +4,16 @@ import java.util.Map;
 import java.util.HashMap;
 
 class Environment {
+  final Environment enclosing;
   private final Map<String, Object> values = new HashMap<>();
+
+  Environment() {
+    enclosing = null;
+  }
+
+  Environment(Environment enclosing) {
+    this.enclosing = enclosing;
+  }
 
   /** Adds a new variable to the environment */
   void define(String name, Object value) {
@@ -17,6 +26,10 @@ class Environment {
       return values.get(name.lexeme);
     }
 
+    // If this isn't the global scope, checking parent scope for var
+    if (enclosing != null)
+      return enclosing.get(name);
+
     throw new RuntimeError(name, String.format("Undefined variable '%s'.", name.lexeme));
   }
 
@@ -24,6 +37,13 @@ class Environment {
   void assign(Token name, Object value) {
     if (values.containsKey(name.lexeme)) {
       values.put(name.lexeme, value);
+      return;
+    }
+
+    // If variable is not defined in the current scope,
+    // delegate assignment to the parent scope
+    if (enclosing != null) {
+      enclosing.assign(name, value);
       return;
     }
 
