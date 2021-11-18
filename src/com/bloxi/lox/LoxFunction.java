bae4dbb2d573f9404cce8afa985004a32e73cb8f
Bihan Viranga <bihanviranga@gmail.com>
2021-11-19 01:29:58 +0530

    Added basic function closures
    79 tests passed. 160 tests failed.

--- src/com/bloxi/lox/LoxFunction.java
+++ src/com/bloxi/lox/LoxFunction.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
+  private final Environment closure;
 
-  LoxFunction(Stmt.Function declaration) {
+  LoxFunction(Stmt.Function declaration, Environment closure) {
     this.declaration = declaration;
+    this.closure = closure;
   }
 
   @Override
   public Object call(Interpreter interpreter, List<Object> arguments) {
-    Environment environment = new Environment(interpreter.globals);
+    Environment environment = new Environment(closure);
 
     // define function params as variables in the environment
     for (int i = 0; i < declaration.params.size(); i++) {
